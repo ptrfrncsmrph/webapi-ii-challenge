c815614c4aac765254a2fae77ec26785ec3587d0
Peter Murphy <peter.francis.murphy@gmail.com>
2019-03-19 17:58:35 -0400

    Add post post and get all posts

--- api/posts/router.js
+++ api/posts/router.js
@@ -4,15 +4,32 @@ const db = require("../../data/db")
 
 const router = express.Router()
 
-router.get("/", async (req, res) => {
+router.post("/", async (req, res) => {
+  const { title, contents } = req.body
+  if (title == null || contents == null) {
+    res.status(400).json({
+      message: "Please provide title and contents for the post."
+    })
+  }
   try {
-    const hubs = await db.find(req.query)
-    res.status(200).json(hubs)
+    const { id } = await db.insert({ title, contents })
+    res.status(200).json({ title, contents, id })
   } catch (error) {
-    // log error to database
     console.log(error)
     res.status(500).json({
-      message: "Error retrieving the hubs"
+      message: "Error creating the post."
+    })
+  }
+})
+
+router.get("/", async (_req, res) => {
+  try {
+    const posts = await db.find()
+    res.status(200).json(posts)
+  } catch (error) {
+    console.log(JSON.stringify(error, null, 2))
+    res.status(500).json({
+      message: "Error finding the posts."
     })
   }
 })
